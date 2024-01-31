343cac7d16597b179d1c74e57f4542a0ad5589e3
Vaibhav <vaibhavknw1@gmail.com>
2024-01-31 14:22:18 +0530

    new assessment

--- src/component/CardComponent/Card.jsx
+++ src/component/CardComponent/Card.jsx
@@ -5,11 +5,11 @@ const Card = ({data}) => {
     console.log(imgPath)
   return (
    <div className='col'>
-     <div className="card p-3 bg-light border border-0 shadow-sm" style={{ height:'420px' }}>
+     <div className="card bg-light border border-0 shadow-sm" style={{ height:'420px' }}>
          <div className="card-img w-100 d-flex align-items-center justify-content-center p-0">
             <img style={{height:'200px', width:'400px'}} src ='src/assets/not.png'alt="" />
           </div>
-          <div className='mt-2'>
+          <div className='mt-2 p-3'>
             <h4 className='fs-6'>{data.Name}</h4>
             <p>
               {data.ShortDesc}
